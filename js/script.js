15c61f0e6c3c0910b8c047ba05456010a2fac366
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2023-10-12 13:32:06 +0530

    Slight update

--- js/script.js
+++ js/script.js
@@ -37,9 +37,11 @@ function curtainRaise() {
   var panel_left = document.querySelector('.panel-left');
   var panel_right = document.querySelector('.panel-right');
   var curtain_wrapper = document.querySelector('.curtain-wrapper');
+  var curtain = document.querySelector('.curtain');
   panel_left.classList.remove('panel-left');
   panel_right.classList.remove('panel-right');
   panel_left.classList.add('panel-left__move');
   panel_right.classList.add('panel-right__move');
   curtain_wrapper.style.setProperty('z-index', -100);
+  curtain.style.setProperty('display', 'none');
 }
